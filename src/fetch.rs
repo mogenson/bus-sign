ed28c81a6c7fa79fe9b0378b25ca3d23cebb0ca4
Michael Mogenson <mogenson@google.com>
2024-12-18 13:13:45 -0500

    split lib up into separate modules

---
+++ src/fetch.rs
@@ -0,0 +1,59 @@
+use core::str::from_utf8;
+
+use embassy_net::dns::DnsSocket;
+use embassy_net::tcp::client::{TcpClient, TcpClientState};
+use log::*;
+use reqwless::client::HttpClient;
+use reqwless::request::Method;
+use serde::Deserialize;
+
+pub async fn http_get_json<'a, T>(
+    stack: embassy_net::Stack<'_>,
+    rx_buffer: &'a mut [u8],
+    url: &str,
+) -> Option<T>
+where
+    T: Deserialize<'a>,
+{
+    let client_state = TcpClientState::<1, 1024, 1024>::new();
+    let tcp_client = TcpClient::new(stack, &client_state);
+    let dns_client = DnsSocket::new(stack);
+
+    let mut http_client = HttpClient::new(&tcp_client, &dns_client);
+
+    info!("connecting to {}", &url);
+
+    let mut request = match http_client.request(Method::GET, url).await {
+        Ok(req) => req,
+        Err(e) => {
+            error!("Failed to make HTTP request: {:?}", e);
+            return None;
+        }
+    };
+
+    let response = match request.send(rx_buffer).await {
+        Ok(resp) => resp,
+        Err(_e) => {
+            error!("Failed to send HTTP request");
+            return None;
+        }
+    };
+
+    let body = match from_utf8(response.body().read_to_end().await.unwrap()) {
+        Ok(b) => b,
+        Err(_e) => {
+            error!("Failed to read response body");
+            return None;
+        }
+    };
+    info!("Response body: {:?}", &body);
+
+    let bytes = body.as_bytes();
+    match serde_json_core::de::from_slice::<T>(bytes) {
+        Ok((output, _used)) => Some(output),
+        Err(_e) => {
+            error!("Failed to parse response body");
+            None
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,10 @@
 #![no_main]
 #![allow(async_fn_in_trait)]
 
-use core::str::from_utf8;
-
-use bus_sign::Timestamp;
+use bus_sign::fetch::http_get_json;
+use bus_sign::timestamp::Timestamp;
 use cyw43_pio::PioSpi;
 use embassy_executor::Spawner;
-use embassy_net::dns::DnsSocket;
-use embassy_net::tcp::client::{TcpClient, TcpClientState};
 use embassy_net::{Config, StackResources};
 use embassy_rp::bind_interrupts;
 use embassy_rp::clocks::RoscRng;
@@ -20,8 +17,6 @@ use embassy_rp::usb::{Driver, InterruptHandler as UsbInterruptHandler};
 use embassy_time::{Duration, Timer};
 use log::*;
 use rand::RngCore;
-use reqwless::client::HttpClient;
-use reqwless::request::Method;
 use serde::Deserialize;
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
@@ -142,63 +137,12 @@ async fn main(spawner: Spawner) {
 
         let mut rx_buffer = [0; 4096];
         let url = "http://worldtimeapi.org/api/timezone/America/New_York";
-        if let Some(json) = http_get::<ApiResponse>(stack, &mut rx_buffer, url).await {
+        if let Some(json) = http_get_json::<ApiResponse>(stack, &mut rx_buffer, url).await {
             let timestamp = Timestamp::parse(json.datetime).unwrap();
             info!("Timestamp: {:?}", timestamp);
             rtc.set_datetime(timestamp.into()).unwrap();
         }
 
         Timer::after(Duration::from_secs(5)).await;
-    }
-}
-
-async fn http_get<'a, T>(
-    stack: embassy_net::Stack<'_>,
-    rx_buffer: &'a mut [u8],
-    url: &str,
-) -> Option<T>
-where
-    T: Deserialize<'a>,
-{
-    let client_state = TcpClientState::<1, 1024, 1024>::new();
-    let tcp_client = TcpClient::new(stack, &client_state);
-    let dns_client = DnsSocket::new(stack);
-
-    let mut http_client = HttpClient::new(&tcp_client, &dns_client);
-
-    info!("connecting to {}", &url);
-
-    let mut request = match http_client.request(Method::GET, url).await {
-        Ok(req) => req,
-        Err(e) => {
-            error!("Failed to make HTTP request: {:?}", e);
-            return None;
-        }
-    };
-
-    let response = match request.send(rx_buffer).await {
-        Ok(resp) => resp,
-        Err(_e) => {
-            error!("Failed to send HTTP request");
-            return None;
-        }
-    };
-
-    let body = match from_utf8(response.body().read_to_end().await.unwrap()) {
-        Ok(b) => b,
-        Err(_e) => {
-            error!("Failed to read response body");
-            return None;
-        }
-    };
-    info!("Response body: {:?}", &body);
-
-    let bytes = body.as_bytes();
-    match serde_json_core::de::from_slice::<T>(bytes) {
-        Ok((output, _used)) => Some(output),
-        Err(_e) => {
-            error!("Failed to parse response body");
-            None
-        }
     }
 }
